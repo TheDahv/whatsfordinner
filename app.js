db15f067273816d7bf7958625eab6cdc2d33f953
David Pierce <david.dean.pierce@gmail.com>
2011-06-01 00:51:23 -0700

    trying to set up app with mongohq

--- app.js
+++ app.js
@@ -45,7 +45,8 @@ app.configure('test', function() {
 });
 
 app.configure('production', function() {
-  app.set('db-uri', 'mongodb://localhost/whatsfordinner');
+  var connStr = process.env.MONGOHQ_URL || 'mongodb://localhost/whatsfordinner'; 
+  app.set('db-uri', connStr);
 });
 
 db = mongoose.connect(app.set('db-uri'));
